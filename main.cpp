713cad6f55760a5df152eb69887719031bd75e58
AshunMotley <ashunmotley25@gmail.com>
2025-04-29 12:43:55 -0500

    made sure all test pass with the code created

--- main.cpp
+++ main.cpp
@@ -6,24 +6,25 @@
 using namespace std;
 
 int main() {
-    //changes
-    // Test Point
+    //changes(debugging)
+    
+    // Testin the  Point
     Point p1(1.2, 3.4);
     Point p2(-5.6, 7.8);
     assert(p1.toString() == "X: 1.2, Y: 3.4");
     assert(p2.toString() == "X: -5.6, Y: 7.8");
 
-    // Test Line
+    // Testing  Line
     Line l1(p1, p2);
     assert(l1.toString() == "Point 1: [X: 1.2, Y: 3.4], Point 2: [X: -5.6, Y: 7.8]");
 
     // Length
     double len = l1.length();
-    assert(len > 0);  // Should be positive
+    assert(len > 0);
 
     // Contains
-    Point mid((p1.getX() + p2.getX()) / 2, (p1.getY() + p2.getY()) / 2);
-    assert(l1.contains(mid));
+    Point middle((p1.getX() + p2.getX()) / 2, (p1.getY() + p2.getY()) / 2);
+    assert(l1.contains(middle));
 
     // Parallel test
     Line l2(Point(0, 0), Point(1, 1));
@@ -35,6 +36,6 @@ int main() {
     Line l5(Point(0, 4), Point(4, 0));
     assert(l4.intersecting(l5));
 
-    cout << "All Line and Point tests passed!" << endl;
+    cout << "All the test have passed" << endl;
     return 0;
 }
